aac6df06f103c05ef8f827fa822183f39252cb53
Aadi9910 <informarcialaditya@gmail.com>
2022-12-11 13:23:38 +0530

    almost

--- src/App.js
+++ src/App.js
@@ -1,29 +1,107 @@
+import { useReducer } from "react";
+import DigitBtn from "./DigitBtn";
+import OperationButton from "./OperationButton";
 import "./App.css";
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentoperand === "0") {
+        return state;
+      }
+      if (payload.digit === "." && state.currentoperand.includes(".")) {
+        return state;
+      }
+      return {
+        ...state,
+        currentoperand: `${state.currentoperand || ""}${payload.digit}`,
+      };
 
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentoperand == null && state.previousOperand == null) {
+        return state;
+      }
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentoperand,
+          currentoperand: null,
+        };
+      }
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentoperand: null,
+      };
+    case ACTIONS.CLEAR:
+      return {};
+  }
+}
+function evaluate(currentoperand, previousOperand, operation) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentoperand);
+  if (isNaN(prev) || isNaN(current)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "/":
+      computation = prev / current;
+      break;
+  }
+}
 function App() {
+  const [{ currentoperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   return (
-    <div className="App-header calculator-grid">
+    <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
-        <button className="span-two">AC</button>
-        <button>DEL</button>
-        <button>/</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button className="span-two">=</button>
+        <div className="previous-operand">
+          {previousOperand}
+          {operation}
+        </div>
+        <div className="current-operand">{currentoperand}</div>
       </div>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
+      <button>DEL</button>
+      <OperationButton operation="/" dispatch={dispatch} />
+      <DigitBtn digit="1" dispatch={dispatch} />
+      <DigitBtn digit="2" dispatch={dispatch} />
+      <DigitBtn digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitBtn digit="4" dispatch={dispatch} />
+      <DigitBtn digit="5" dispatch={dispatch} />
+      <DigitBtn digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitBtn digit="7" dispatch={dispatch} />
+      <DigitBtn digit="8" dispatch={dispatch} />
+      <DigitBtn digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitBtn digit="." dispatch={dispatch} />
+      <DigitBtn digit="0" dispatch={dispatch} />
+      <button className="span-two">=</button>
     </div>
   );
 }
